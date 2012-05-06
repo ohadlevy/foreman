6ca3827a1b4ccd8cfdd665ddd549ebe3a8c07e2c
Ohad Levy <ohadlevy@gmail.com>
2012-05-06 11:41:49 +0300

    added instance properties

--- lib/foreman/model/ec2.rb
+++ lib/foreman/model/ec2.rb
@@ -10,12 +10,39 @@ module Foreman::Model
     def vm_instance_defaults
       {
         :flavor_id => "m1.small",
+        :use_image => true,
       }
     end
 
+    def find_vm_by_uuid uuid
+      client.servers.get(uuid)
+    rescue Fog::Compute::AWS::Error
+      raise(ActiveRecord::RecordNotFound)
+    end
+
+    def create_vm args = {}
+      super(args)
+    end
+
+    def key_pairs
+      client.key_pairs.map(&:name)
+    end
+
+    def security_groups
+      client.security_groups.map(&:name)
+    end
+
     def regions
-     return [] if user.blank? or password.blank?
-     @regions ||= client.describe_regions.body["regionInfo"].map{|r| r["regionName"]}
+      return [] if user.blank? or password.blank?
+      @regions ||= client.describe_regions.body["regionInfo"].map{|r| r["regionName"]}
+    end
+
+    def zones
+      @zones ||= client.describe_availability_zones.body["availabilityZoneInfo"].map{|r| r["zoneName"] if r["regionName"] == region}.compact
+    end
+
+    def flavors
+      client.flavors
     end
 
     def test_connection
