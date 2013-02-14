558a13973b1ded6d9f6af556c91b37de25eb2e2d
Ohad Levy <ohadlevy@gmail.com>
2013-02-14 12:27:23 +0200

    tryign to set default type hack?

--- app/models/host.rb
+++ app/models/host.rb
@@ -3,6 +3,9 @@ module Host
   def self.method_missing(method, *args, &block)
     super
   rescue NoMethodError
+    if [:create, :new, :create!].includes?(method)
+      args[:type] ||= 'Host::Managed'
+    end
     Host::Base.send(method,*args, &block)
   end
 
