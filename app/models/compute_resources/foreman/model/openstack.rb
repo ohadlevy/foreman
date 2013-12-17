d420159cacb1c82780e1eeb1e30156a57833ed37
Ohad Levy <ohadlevy@gmail.com>
2013-12-17 15:26:54 -0500

    fixes #3902 - [Openstack] allow selection of internal networks
    This patch allows the user to select the internal network when deploying
    a new instance in openstack.
    
    In Grizlly, this was done automaticilly, with a nasty side effect, that
    all internal networks would get assigned (didnt see that as i had only
    one internal network when i was testing), starting in Havana, this is no
    longer possible, and the vm creation is rejected.
    
    as a bonus, we now retain the floating ip selection if the form had an
    error.

--- app/models/compute_resources/foreman/model/openstack.rb
+++ app/models/compute_resources/foreman/model/openstack.rb
@@ -49,8 +49,15 @@ module Foreman::Model
       client.addresses.get_address_pools.map { |p| p["name"] }
     end
 
+    def internal_networks
+      network_client.networks.all.select { |net| !net.router_external }
+    end
+
     def create_vm(args = {})
       network = args.delete(:network)
+      # fix internal network format for fog.
+      args[:nics].delete_if(&:blank?)
+      args[:nics].map! {|nic| { 'net_id' => nic } }
       vm      = super(args)
       if network.present?
         address = allocate_address(network)
@@ -102,10 +109,19 @@ module Foreman::Model
 
     def client
       @client ||= ::Fog::Compute.new(:provider           => :openstack,
-                                     :openstack_api_key  => password  ,
-                                     :openstack_username => user      ,
-                                     :openstack_auth_url => url       ,
+                                     :openstack_api_key  => password,
+                                     :openstack_username => user,
+                                     :openstack_auth_url => url,
                                      :openstack_tenant   => tenant)
+    end
+
+    def network_client
+      @network_client ||= ::Fog::Network.new(:provider           => :openstack,
+                                             :openstack_api_key  => password,
+                                             :openstack_username => user,
+                                             :openstack_auth_url => url,
+                                             :openstack_tenant   => tenant)
+
     end
 
     def setup_key_pair
