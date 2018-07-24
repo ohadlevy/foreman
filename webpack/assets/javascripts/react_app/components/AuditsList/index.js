f1e00905718162bca775133de3a045fbb5661a6e
Ohad Levy <ohadlevy@gmail.com>
2018-07-24 16:00:31 +0300

    minor cleanups

--- webpack/assets/javascripts/react_app/components/AuditsList/index.js
+++ webpack/assets/javascripts/react_app/components/AuditsList/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { ListView, Row, Col } from 'patternfly-react';
 
+const heading = audit => audit.username;
+
+const style = { color: '#999' };
+const description = audit => (
+  <ListView.Description>
+    <ListView.DescriptionHeading style={style}>
+      ({audit.remote_address})
+    </ListView.DescriptionHeading>
+    <ListView.DescriptionText>{audit.action}</ListView.DescriptionText>
+    <ListView.DescriptionText>{audit.auditable_type}</ListView.DescriptionText>
+    <ListView.AdditionalInfo>{audit.auditable_name}</ListView.AdditionalInfo>
+  </ListView.Description>
+);
+
 export default ({ data: { audits } }) => (
   <div>
     <ListView>
-      {audits.map(({ audit }, index) => (
+      {audits.map(({ audit }) => (
         <ListView.Item
-          key={index}
+          key={audit.id}
           //   actions={renderActions(item.actions)}
-          checkboxInput={<input type="checkbox" />}
-          leftContent={<ListView.Icon name="plane" />}
-          //   additionalInfo={renderAdditionalInfoItems(item.properties)}
-          heading={`${audit.username}(${audit.remote_address})`}
-          description="a very long description"
+          // checkboxInput={<input type="checkbox" />}
+          // leftContent={<ListView.Icon name="plane" />}
+          // additionalInfo={renderAdditionalInfoItems(item.properties)}
+          heading={heading(audit)}
+          description={description(audit)}
           stacked
         >
           <Row>
-            <Col sm={11}>{audit.expandedContentText}</Col>
+            <Col sm={11}>{audit.request_uuid}</Col>
           </Row>
         </ListView.Item>
       ))}
