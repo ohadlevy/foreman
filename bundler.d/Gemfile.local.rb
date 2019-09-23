667f69156425770f82f1eac736b8eeee644008f5
Ohad Levy <ohadlevy@gmail.com>
2019-09-23 08:38:54 +0300

    adds more plugins to my image.

---
+++ bundler.d/Gemfile.local.rb
@@ -0,0 +1,7 @@
+gem "foreman_discovery", git: "https://github.com/theforeman/foreman_discovery.git", branch: 'develop'
+gem "foreman_dhcp_browser", :git => "https://github.com/theforeman/foreman_dhcp_browser.git"
+gem 'foreman_remote_execution', :git => 'https://github.com/theforeman/foreman_remote_execution.git'
+gem 'foreman_ansible', :git => 'https://github.com/theforeman/foreman_ansible.git'
+gem 'foreman_expire_hosts'
+gem 'foreman_cockpit', :git => 'https://github.com/theforeman/foreman_cockpit.git'
+gem 'foreman_openscap', :git => 'https://github.com/theforeman/foreman_openscap.git'
